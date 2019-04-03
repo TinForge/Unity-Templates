6877e7939919afcabfa1ce4b2ff12bf562678116
TinForge <29712552+TinForge@users.noreply.github.com>
2019-04-03 15:31:38 -0400

    Add files via upload

---
+++ AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Static singleton to decouple audio and component referencing.
+/// All BUTTON clicks sounds are hooked up using a custom editor gui script.
+/// </summary>
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+
+    [SerializeField] private AudioSource showcaseSwitch;
+    public static AudioSource ShowcaseSwitch;
+
+    [SerializeField] private AudioSource teleport;
+    public static AudioSource Teleport;
+
+    [SerializeField] private AudioSource pointerDown;
+    public static AudioSource PointerDown;
+
+    [SerializeField] private AudioSource pointerUp;
+    public static AudioSource PointerUp;
+
+    [SerializeField] private AudioSource pointerEnter;
+    public static AudioSource PointerEnter;
+
+    [SerializeField] private AudioSource pointerExit;
+    public static AudioSource PointerExit;
+
+
+    void Awake()
+    {
+        instance = this;
+
+        ShowcaseSwitch = showcaseSwitch;
+        Teleport = teleport;
+        PointerDown = pointerDown;
+        PointerUp = pointerUp;
+        PointerEnter = pointerEnter;
+        PointerExit = pointerExit;
+    }
+
+    public static void PlaySound(AudioSource sound)
+    {
+        sound.Play();
+    }
+}
